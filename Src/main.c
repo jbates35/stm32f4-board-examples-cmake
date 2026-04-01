41804d84fb75fb9ffac0f088e1293e91f7463cb9
Jimmy Bates <jbates.productions@gmail.com>
2026-04-01 00:25:37 -0700

    GPIO setup added for UART example

--- Src/main.c
+++ Src/main.c
@@ -10,6 +10,8 @@
 #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+void setup_uart();
+
 int _write(int le, char* ptr, int len) {
   int DataIdx;
   for (DataIdx = 0; DataIdx < len; DataIdx++) {
@@ -31,13 +33,31 @@ int _write(int le, char* ptr, int len) {
   } while (0)
 
 int main(void) {
+  uint8_t usart_tx_byte_send = 8;
+  setup_uart();
   for (;;) {
     WAIT(MEDIUM);
+    usart_tx_byte_blocking(USART2, usart_tx_byte_send);
   }
 }
 
 void setup_uart() {
-  USARTConfig_t uart_cfg = {.baud_rate = USART_BAUD_RATE_115200,
+  GPIO_peri_clock_control(GPIOA, GPIO_CLOCK_ENABLE);
+  GPIOConfig_t default_gpio_cfg = {.mode = GPIO_MODE_ALTFN,
+                                   .speed = GPIO_SPEED_HIGH,
+                                   .float_resistor = GPIO_PUPDR_PULLUP.output_type = GPIO_OP_TYPE_PUSHPULL,
+                                   .alt_func_num = 7};
+
+  GPIOHandle_t uart_tx = {.p_GPIO_addr = GPIOA, .cfg = default_gpio_cfg};
+  uart_tx.cfg.pin_number = 2;
+  GPIO_init(&uart_tx);
+
+  GPIOHandle_t uart_rx = {.p_GPIO_addr = GPIOA, .cfg = default_gpio_cfg};
+  uart_rx.cfg.pin_number = 3;
+  GPIO_init(&uart_rx);
+
+  usart_peri_clock_control(USART2, USART_ENABLE);
+  USARTConfig_t uart_cfg = {.baud_rate = USART_BAUD_RATE_9600,
                             .peri_clock_freq_hz = 16E6,
                             .en_on_start = USART_ENABLE,
                             .mode = USART_MODE_RX_ONLY,
@@ -46,4 +66,9 @@ void setup_uart() {
                             .stop_bit_count = USART_STOP_BITS_ONE,
                             .word_length = USART_WORD_LENGTH_8_BIT_DATA,
                             .synchronous = USART_ASYNCHRONOUS};
+
+  USARTHandle_t usart_handle = {.addr = USART2, .cfg = uart_cfg
+
+  };
+  usart_init(&usart_handle);
 }
